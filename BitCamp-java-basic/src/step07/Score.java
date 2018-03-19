aab6b937cec008992c0896f554138b47a0e7a2f0
Leewangro <dldhkdfh@naver.com>
2018-03-19 11:28:15 +0900

    c

---
+++ BitCamp-java-basic/src/step07/Score.java
@@ -0,0 +1,13 @@
+package step07;
+
+
+public class Score { // 사용자 정의 데이터 타입 설계
+                // = > 학생의 성적 데이터를 담을 새로운 구조의 메모리를 설계한다.
+    public String name;
+    public int age;
+    public int kor;
+    public int eng;
+    public int math;
+    public float average;
+    public int sum;
+}
