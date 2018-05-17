7a677eba774e2cfb61a32de1b4b3eb5fb4f3f613
Leewangro <dldhkdfh@naver.com>
2018-05-17 14:16:59 +0900

    2018-05-17

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/teammember/TeamMemberAddServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/teammember/TeamMemberAddServlet.java
@@ -47,7 +47,7 @@ public class TeamMemberAddServlet extends HttpServlet {
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset='UTF-8'>");
-        out.printf("<meta http-equiv='Refresh' content='1;url=../view?name=%s'>\n",teamName);
+        out.printf("<meta http-equiv='Refresh' content='1;url=../view?name=%s'>", teamName);
         
         out.println("<title>팀 등록</title>");
         out.println("</head>");
@@ -56,21 +56,16 @@ public class TeamMemberAddServlet extends HttpServlet {
                     
         try {
             Team team = teamDao.selectOne(teamName);
-            if (team == null) {
-                out.printf("<p>%s 팀은 존재하지 않습니다.</p>", teamName);
-                return;
-            }
+            if (team == null)
+                throw new Exception("<p>" + teamName + " 팀은 존재하지 않습니다.</p>");
             
             Member member = memberDao.selectOne(memberId);
-            if (member == null) {
-                out.printf("<p>%s 회원은 없습니다.<p>", memberId);
-                return;
-            }
+            if (member == null)
+                throw new Exception("<p>" + memberId + " 회원은 없습니다.<p>");
             
-            if (teamMemberDao.isExist(teamName, memberId)) {
-                out.println("<p>이미 등록된 회원입니다.<p>");
-                return;
-            }
+            if (teamMemberDao.isExist(teamName, memberId))
+                throw new Exception("<p>이미 등록된 회원입니다.<p>");
+            
         } catch(Exception e) {
             out.println("<p>검색실패<p>");
             e.printStackTrace(out);
@@ -80,20 +75,10 @@ public class TeamMemberAddServlet extends HttpServlet {
             teamMemberDao.insert(teamName, memberId);
             out.println("<p>팀에 회원을 추가하였습니다.</p>");
         } catch(Exception e) {
-            out.println("<p>등록 실패<p>");
+            out.printf("<p>%s<p>", e.getMessage());
             e.printStackTrace(out);
         }
         out.println("</body>");
         out.println("</html>");
     }
 }
-
-//ver 28 - 네트워크 버전으로 변경
-//ver 26 - TeamMemberController에서 add() 메서드를 추출하여 클래스로 정의.
-//ver 23 - @Component 애노테이션을 붙인다.
-//ver 18 - ArrayList가 적용된 TeamMemberDao를 사용한다.
-//ver 17 - TeamMemberDao 클래스를 사용하여 팀 멤버의 아이디를 관리한다.
-//ver 16 - 인스턴스 변수를 직접 사용하는 대신 겟터, 셋터 사용.
-// ver 15 - 팀 멤버를 등록, 조회, 삭제할 수 있는 기능 추가. 
-// ver 14 - TeamDao를 사용하여 팀 데이터를 관리한다.
-// ver 13 - 시작일, 종료일을 문자열로 입력 받아 Date 객체로 변환하여 저장.
