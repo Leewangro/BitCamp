33286f6ac7a61b6a0ae41ad73b548843cda63cf7
Leewangro <dldhkdfh@naver.com>
2018-05-24 09:13:23 +0900

    2018-05-24

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/ErrorServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/ErrorServlet.java
@@ -1,4 +1,4 @@
-// 오류 처리 작업을 수행하는 서블릿 - forward 적용
+// 모든 페이지의 공통 헤더를 출력하는 서블릿
 package bitcamp.java106.pms.servlet;
 
 import java.io.IOException;
@@ -9,32 +9,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bitcamp.java106.pms.domain.Member;
 
 @SuppressWarnings("serial")
-@WebServlet("/error")
+@WebServlet("/header")
 public class ErrorServlet extends HttpServlet{
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        
+        // 이 서블릿을 include하는 쪽에서 출력스트림의 콘텐트 타입을 설정하기 때문에
+        // 이 서블릿에서는 콘텐트 타입을 설정할 필요가 없다
+        //response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset='UTF-8'>");
-        String refererUrl= request.getHeader("Referer");
-        if (refererUrl != null) {
-        out.printf("<meta http-equiv='Refresh' content='5;url=%s'>",refererUrl
-                );
+        
+        HttpSession session = request.getSession();
+        Member loginUser = (Member)session.getAttribute("loginUser");
+        out.println("<div id = 'header'>");
+        if(loginUser != null) {
+            out.printf("%s\n",loginUser.getId());
+            out.printf("<a href = '%s/auth/logout'>로그아웃</a>",request.getContextPath());
+        } else {
+            out.printf("<a href = '%s/auth/login'>로그인</a>",request.getContextPath());
         }
-        out.println("<title>실행 오류 </title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.printf("<h1>%s</h1>\n",request.getAttribute("title"));
-        out.println("<pre>");
-        Exception e = (Exception)request.getAttribute("error");
-        e.printStackTrace(out);
-        out.println("</pre>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println("</div>");
     }
 }
