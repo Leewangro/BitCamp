e5a7bdae238356b691163d5468accda783827c78
Leewangro <dldhkdfh@naver.com>
2018-08-10 14:57:15 +0900

    # WARNING: head commit changed in the meantime
    2018-07-04

--- bitcamp-facebook-chatbot/app.js
+++ bitcamp-facebook-chatbot/app.js
@@ -102,7 +102,7 @@ app.get('/webhook', (req, res) => {
 
 function handleMessage(sender_psid, received_message) {
   let response;
-  
+   
   // Checks if the message contains text
   if (received_message.text) {    
     // Create the payload for a basic text message, which
